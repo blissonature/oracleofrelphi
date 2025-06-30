28ca59bb5696aa27962ab117c5849428f17c1b3a
blissonature <blissonature@gmail.com>
2025-06-30 01:24:15 +0000

    starting over from scratch

--- src/lib/i18n.js
+++
@@ -1,4 +0,0 @@
-import { setLocale, locale } from '$paraglide/runtime';
-import { availableLanguageTags } from '$paraglide';
-
-export { setLocale, availableLanguageTags, locale };
--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import LanguageSelector from '$lib/LanguageSelector.svelte';
-</script>
-
-<main>
-  <h1>Oracle of Relphi</h1>
-  <LanguageSelector />
-</main>
