6fa02e9a8f0594c1c1e48cb58c213380a7a72b0a
blissonature <blissonature@gmail.com>
2026-07-13 14:44:37 -0600

    Load Static and Dynamic Sky Chart controls

--- navloader.js
+++ navloader.js
@@ -107,7 +107,8 @@
       appendScript('standardize-zodiac-wheels.js?v=2');
     }
     if (/(^|\/)sky-chart\.html$/.test(window.location.pathname)) {
-      appendScript('sky-chart-aspect-duration-fix.js?v=1');
+      appendScript('sky-chart-static-dynamic.js?v=1');
+      appendScript('sky-chart-aspect-duration-fix.js?v=2');
     }
   }
 
@@ -188,7 +189,7 @@
       return;
     }
 
-    fetch('nav.html?v=10')
+    fetch('nav.html?v=11')
       .then(function (response) {
         if (!response.ok) throw new Error('Could not load nav.html');
         return response.text();
