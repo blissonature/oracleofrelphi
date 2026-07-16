594a08ea0fe9cd29a8f5750567f32d6579755625
blissonature <blissonature@gmail.com>
2026-07-16 15:06:00 -0600

    Load the consolidated Wizard state controller

--- navloader.js
+++ navloader.js
@@ -75,6 +75,7 @@
         'sky-chart-active-sky-controls.js?v=2',
         'sky-chart-core-target-fix.js?v=1',
         'sky-chart-language-cleanup.js?v=5',
+        'sky-chart-wizard-state-controller.js?v=1',
         'sky-chart-wizard-ui-state-fix.js?v=3'
       ].forEach(appendScript);
       const preview = new URLSearchParams(location.search).get('preview');
