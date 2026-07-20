ec3cbc5c63a8409a5348e64c063ef004e6b39b3e
blissonature <blissonature@gmail.com>
2026-07-20 14:21:50 -0600

    Load tangential wheel collision correction

--- navloader.js
+++ navloader.js
@@ -148,7 +148,7 @@
         'sky-chart-ph-glyph-style-v1.js?v=2',
         'relphi-glyph-bubbles.js?v=2',
         'sky-chart-provenance-fix.js?v=1',
-        'sky-chart-wheel-label-collision-v1.js?v=2'
+        'sky-chart-wheel-label-collision-v1.js?v=3'
       ].forEach(function (src) { appendScript(src); });
 
       appendScript('sky-chart-builder-v4-unlock.js?v=1');
