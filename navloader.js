ec30d2f372f97079a42f80bb8109a1336aac5957
blissonature <blissonature@gmail.com>
2026-07-24 21:04:57 -0600

    Load canonical relationship UI on Sky Chart

--- navloader.js
+++ navloader.js
@@ -173,6 +173,7 @@
         'sky-chart-static-dynamic.js?v=2',
         'sky-chart-aspect-duration-fix.js?v=2',
         'sky-chart-relationship-language.js?v=5',
+        'sky-chart-canonical-relationship-ui-v1.js?v=1',
         'sky-chart-related-relationships-v2.js?v=2',
         'sky-chart-sign-cusps-v1.js?v=1',
         'sky-chart-provenance-fix.js?v=1',
