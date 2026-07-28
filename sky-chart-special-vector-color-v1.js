12605e4e4be6d32aa96e09ea124b0369a371824e
blissonature <blissonature@gmail.com>
2026-07-28 10:45:20 -0600

    Load compact aspect-enabled Sky Cards

--- sky-chart-special-vector-color-v1.js
+++ sky-chart-special-vector-color-v1.js
@@ -71,7 +71,7 @@
       load('sky-chart-workspace-desktop-width-v1.js?v=3');
       load('sky-chart-inline-card-editor-v1.js?v=2');
       load('sky-chart-selected-relationship-layout-v1.js?v=4');
-      load('sky-chart-skinny-cards-v1.js?v=3', function () {
+      load('sky-chart-skinny-cards-v1.js?v=4', function () {
         load('sky-chart-skinny-cluster-context-v1.js?v=1');
         load('sky-chart-skinny-heptagram-v1.js?v=1');
       });
