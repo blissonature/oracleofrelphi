691616d49bb764e99ccb3239a137b128d5e81371
blissonature <blissonature@gmail.com>
2026-07-30 15:39:35 -0600

    Load uncached sky-card graphic hierarchy

--- sky-chart-special-vector-color-v1.js
+++ sky-chart-special-vector-color-v1.js
@@ -77,7 +77,7 @@
       load('sky-chart-skinny-cards-v1.js?v=4', function () {
         load('sky-chart-skinny-cluster-context-v1.js?v=1');
         load('sky-chart-skinny-heptagram-v1.js?v=1', function () {
-          load('sky-chart-skinny-graphic-hierarchy-v1.js?v=1');
+          load('sky-chart-skinny-graphic-hierarchy-v2.js');
         });
         load('sky-chart-skinny-aspect-anchor-v1.js?v=1');
       });
