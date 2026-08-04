a3a49ed48174b9e2f771095fb621492b9528c764
blissonature <blissonature@gmail.com>
2026-08-03 19:09:59 -0600

    Remove obsolete glyph styling compatibility file

--- sky-chart-ph-glyph-style-v1.js
+++
@@ -1,6 +0,0 @@
-// Compatibility no-op: canonical Sky Chart marker renderer owns glyph visibility and styling.
-(function(){
-  'use strict';
-  if(!/(^|\/)sky-chart\.html$/.test(location.pathname))return;
-  document.getElementById('relphiPhGlyphStyle')?.remove();
-})();
