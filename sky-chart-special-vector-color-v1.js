600fd9fcd5108b7b4c38e49c9e374bcb02875fbe
blissonature <blissonature@gmail.com>
2026-08-03 19:09:42 -0600

    Remove obsolete special-vector glyph compatibility layer

--- sky-chart-special-vector-color-v1.js
+++
@@ -1,6 +0,0 @@
-// Compatibility no-op: canonical marker renderer owns Pluto and Fortune color.
-(function(){
-  'use strict';
-  if(!/(^|\/)sky-chart\.html$/.test(location.pathname))return;
-  document.getElementById('relphiSpecialVectorColorStyle')?.remove();
-})();
