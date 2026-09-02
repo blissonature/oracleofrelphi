dd63b782a24774ec141266c21e78c2adb2a25b82
blissonature <blissonature@gmail.com>
2026-09-02 13:32:48 -0600

    Bust numbered label builder cache

--- navloader.js
+++ navloader.js
@@ -153,10 +153,10 @@
   function loadEnhancements() {
     if (/(^|\/)tarot\.html$/.test(location.pathname)) {
       appendScript('tarot-date-sky-bridge-v1.js?v=2');
-      appendScript('drawing-board-workflow-v2.js?v=28');
+      appendScript('drawing-board-workflow-v2.js?v=29');
       appendScript('drawing-board-interactions-v1.js?v=4');
       appendScript('drawing-board-template-lifecycle-v1.js?v=3', function () {
-        appendScript('drawing-board-spread-prefabs-v1.js?v=13');
+        appendScript('drawing-board-spread-prefabs-v1.js?v=14');
       });
     }
     if (/(^|\/)planetaryhours\.html$/.test(location.pathname)) {
