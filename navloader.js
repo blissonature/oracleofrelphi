cf6fb7b5c17be82c7e0fceaa29b906b7b02a056e
blissonature <blissonature@gmail.com>
2026-09-03 09:03:10 -0600

    Load centered compact Six Polarities geometry

--- navloader.js
+++ navloader.js
@@ -168,7 +168,7 @@
       appendScript('drawing-board-workflow-v2.js?v=60');
       appendScript('drawing-board-interactions-v1.js?v=10');
       appendScript('drawing-board-template-lifecycle-v1.js?v=6', function () {
-        appendScript('drawing-board-spread-prefabs-v1.js?v=33');
+        appendScript('drawing-board-spread-prefabs-v1.js?v=34');
       });
     }
     if (/(^|\/)planetaryhours\.html$/.test(location.pathname)) {
