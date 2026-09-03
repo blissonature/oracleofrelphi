a47032128775129a8de3cfe4f7685e178e19ea89
blissonature <blissonature@gmail.com>
2026-09-03 07:43:28 -0600

    Load non-stretch compact Drawing Board controls

--- navloader.js
+++ navloader.js
@@ -152,7 +152,7 @@
 
   function refreshDrawingBoardControlAssets() {
     const link = document.querySelector('link[href^="drawing-board-workflow-v2.css"]');
-    if (link) link.href = 'drawing-board-workflow-v2.css?v=7';
+    if (link) link.href = 'drawing-board-workflow-v2.css?v=8';
     if (!document.getElementById('relphi-drawing-board-collapse-contract')) {
       const style = document.createElement('style');
       style.id = 'relphi-drawing-board-collapse-contract';
