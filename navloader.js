582fbf3fb933691f77b351ab5b4f0efe52dd3396
blissonature <blissonature@gmail.com>
2026-09-03 12:27:24 -0600

    Refresh Drawing Board edit controls

--- navloader.js
+++ navloader.js
@@ -165,10 +165,10 @@
     if (/(^|\/)tarot\.html$/.test(location.pathname)) {
       refreshDrawingBoardControlAssets();
       appendScript('tarot-date-sky-bridge-v1.js?v=2');
-      appendScript('drawing-board-workflow-v2.js?v=67');
+      appendScript('drawing-board-workflow-v2.js?v=68');
       appendScript('drawing-board-interactions-v1.js?v=12');
       appendScript('drawing-board-template-lifecycle-v1.js?v=7', function () {
-        appendScript('drawing-board-spread-prefabs-v1.js?v=43');
+        appendScript('drawing-board-spread-prefabs-v1.js?v=44');
       });
     }
     if (/(^|\/)planetaryhours\.html$/.test(location.pathname)) {
