9778e55a1371619de56c28636c4cf477f2c55ba2
blissonature <blissonature@gmail.com>
2026-09-22 17:18:20 -0600

    Define structural anchor set for Vocab

--- sky-chart-vocab-tab-v1.js
+++ sky-chart-vocab-tab-v1.js
@@ -129,6 +129,7 @@ const AXIS_STRUCTURES=[
   {left:'north-node',right:'south-node',label:'Nodal polarity'}
 ];
 const STELLIUM_IDS=new Set(['sun','moon','mercury','venus','mars','jupiter','saturn','uranus','neptune','pluto']);
+const STRUCTURAL_ANCHOR_IDS=new Set(AXIS_STRUCTURES.flatMap(axis=>[axis.left,axis.right]));
 const POLARITY_ATTACH_ORB=3;
 const CLUSTER_ORB=3;
 const HARMONIC=()=>window.RelphiHarmonicOrb;
