807243a86fe8c75c8a4d303b76a4886dad3b04ef
blissonature <blissonature@gmail.com>
2026-09-24 02:34:24 -0600

    Bust cache for Moon position suppression

--- navloader.js
+++ navloader.js
@@ -180,7 +180,7 @@
         appendScript('planetary-hours-sect-v1.js?v=4');
       });
       appendScript('planetary-hours-location-prompt.js?v=4');
-      appendScript('planetary-hours-moon-position-v1.js?v=1');
+      appendScript('planetary-hours-moon-position-v1.js?v=2');
       appendScript('planetary-hours-active-time-integrity-v1.js?v=3');
     }
     if (/(^|\/)astrology-foundations\.html$/.test(location.pathname)) {
